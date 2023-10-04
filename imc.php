d47124a1b39fed4e5f1b5b17fb39f355d9b9ee55
LMaverick <91562063+LMaverick@users.noreply.github.com>
2023-10-03 21:32:42 -0300

    teste

---
+++ imc.php
@@ -0,0 +1,27 @@
+
+    <h1>Calculadora de IMC</h1>
+
+    <?php
+    // Variáveis para armazenar peso e altura (valores de exemplo)
+    $peso = 70; // Peso em quilogramas
+    $altura = 1.75; // Altura em metros
+
+    // Calcular o IMC (Índice de Massa Corporal)
+    $imc = $peso / ($altura * $altura);
+
+    // Exibir o resultado
+    echo "Seu peso é: $peso kg<br>";
+    echo "Sua altura é: $altura m<br>";
+    echo "Seu IMC é: " . number_format($imc, 2) . "<br>";
+
+    // Determinar a categoria de peso
+    if ($imc < 18.5) {
+        echo "Você está abaixo do peso.";
+    } elseif ($imc >= 18.5 && $imc < 24.9) {
+        echo "Seu peso está dentro da faixa saudável.";
+    } elseif ($imc >= 25 && $imc < 29.9) {
+        echo "Você está com sobrepeso.";
+    } else {
+        echo "Você está obeso.";
+    }
+    ?>
